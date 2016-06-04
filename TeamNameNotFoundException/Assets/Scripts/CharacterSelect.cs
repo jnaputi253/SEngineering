eb52a7f7f73c71922b51cd71e5797052bbcb0378
Andrew Yoo <happylin1103@gmail.com>
2016-06-04 14:02:17 -0800

    character selection sfx added

--- TeamNameNotFoundException/Assets/Scripts/CharacterSelect.cs
+++ TeamNameNotFoundException/Assets/Scripts/CharacterSelect.cs
@@ -1,20 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterSelect : MonoBehaviour {
+public class CharacterSelect : MonoBehaviour
+{
+    public AudioClip selectCharacter;
+    public AudioClip startGame;
+    public GameObject soundManager;
+    // Use this for initialization
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        if (SoundManager.instance == null)
+            Instantiate(soundManager);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
 
     public void setCharacter(string name)
     {
+        SoundManager.instance.PlaySingle(selectCharacter);
         ApplicationData.characterName = name;
     }
 }
