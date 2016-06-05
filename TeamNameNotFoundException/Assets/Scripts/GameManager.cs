94e505a0fbfd7e87ed701f223f80d23c2b8ac1ef
Juan Naputi <jnaputi253@gmail.com>
2016-06-04 18:55:55 -0700

    Created basic game over scene

--- TeamNameNotFoundException/Assets/Scripts/GameManager.cs
+++ TeamNameNotFoundException/Assets/Scripts/GameManager.cs
@@ -5,22 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
-	{
-		public float levelStartDelay = 2f;						
-		public float turnDelay = 0.1f;							
-		public int playerBattery = 100;
-        private GameObject Tom;
-        public static GameManager instance = null;				
-		[HideInInspector] public bool playersTurn = true;			
-		private Text levelText;									
-		private GameObject levelImage;
-        public AudioClip cheatSound;				
-		private BoardManager boardScript;					
-		private int level = 1;									
-		private List<Enemy> enemies;							
-		private bool enemiesMoving;								
-		private bool doingSetup = true;
-        static bool firstRun = true;
+{
+	public float levelStartDelay = 2f;						
+	public float turnDelay = 0.1f;							
+	public int playerBattery = 100;
+    private GameObject Tom;
+    public static GameManager instance = null;				
+	[HideInInspector] public bool playersTurn = true;			
+	private Text levelText;									
+	private GameObject levelImage;
+    public AudioClip cheatSound;				
+	private BoardManager boardScript;					
+	private int level = 1;									
+	private List<Enemy> enemies;							
+	private bool enemiesMoving;								
+	private bool doingSetup = true;
+    static bool firstRun = true;
     public GameObject bossBackground;
 
     //Cheat code
@@ -45,109 +45,110 @@ public class GameManager : MonoBehaviour
     }
 
     void OnLevelWasLoaded(int index)
-		{
-            if (firstRun)
+	{
+        if (firstRun)
+        {
+            firstRun = false;
+            return;
+        }
+        level++;
+		InitGame();
+	}
+	
+	 void InitGame()
+	{
+		doingSetup = true;
+        cheatEnabled = false;
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        Tom = GameObject.Find("Tom");
+        cheatText = GameObject.Find("CheatText");
+        bossBackground = GameObject.Find("BossBackground");
+        cheatText.SetActive(false);
+        levelText.text = "Level " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+        enemies.Clear();
+        Tom.SetActive(false);
+        bossBackground.SetActive(false);
+        if (level != 10)
+        {
+            boardScript.SetupScene(level);
+            if (level == 1)
             {
-                firstRun = false;
-                return;
+                Tom.SetActive(true);
             }
-            level++;
-			InitGame();
-		}
-		
-		 void InitGame()
-		{
-			doingSetup = true;
-            cheatEnabled = false;
-			levelImage = GameObject.Find("LevelImage");
-			levelText = GameObject.Find("LevelText").GetComponent<Text>();
-            Tom = GameObject.Find("Tom");
-            cheatText = GameObject.Find("CheatText");
-            bossBackground = GameObject.Find("BossBackground");
-            cheatText.SetActive(false);
-            levelText.text = "Level " + level;
-            levelImage.SetActive(true);
-            Invoke("HideLevelImage", levelStartDelay);
-            enemies.Clear();
-            Tom.SetActive(false);
-            bossBackground.SetActive(false);
-            if (level != 10)
-            {
-                boardScript.SetupScene(level);
-                if (level == 1)
-                {
-                    Tom.SetActive(true);
-                }
-            }
-            else
-            {
-                bossBackground.SetActive(true);
-                boardScript.SetupBossScene();
-            }
+        }
+        else
+        {
+            bossBackground.SetActive(true);
+            boardScript.SetupBossScene();
+        }
     }
 
 
     void HideLevelImage()
-		{
-			levelImage.SetActive(false);	
-			doingSetup = false;
-		}
-		
-		void Update()
-		{
-            if (Input.anyKeyDown)
+	{
+		levelImage.SetActive(false);	
+		doingSetup = false;
+	}
+	
+	void Update()
+	{
+        if (Input.anyKeyDown)
+        {
+            if (!cheatEnabled && Input.GetKeyDown(cheatCode[index]))
             {
-                if (!cheatEnabled && Input.GetKeyDown(cheatCode[index]))
-                {
-                    index++;
-                if (index == cheatCode.Length)
-                    cheatEnabled = true;
-                }
-                else {
-                    index = 0;
-                }
+                index++;
+            if (index == cheatCode.Length)
+                cheatEnabled = true;
             }
-            if (cheatEnabled)
-            {
-                SoundManager.instance.PlaySingle(cheatSound);
-                cheatText.SetActive(true);
-                level = 9;
+            else {
+                index = 0;
             }
-            if (playersTurn || enemiesMoving || doingSetup)
-                return;
-            StartCoroutine(MoveEnemies());
-		}
+        }
+        if (cheatEnabled)
+        {
+            SoundManager.instance.PlaySingle(cheatSound);
+            cheatText.SetActive(true);
+            level = 9;
+        }
+        if (playersTurn || enemiesMoving || doingSetup)
+            return;
+        StartCoroutine(MoveEnemies());
+	}
 
-		public void AddEnemyToList(Enemy script)
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add(script);
+	}
+	
+	public void GameOver()
+	{
+		levelText.text = "Your laptop died! You are stuck in the computer forever. GAME OVER.";
+		levelImage.SetActive(true);
+		enabled = false;
+		SceneManager.LoadScene ("GameOverScene");
+	}
+	
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;	
+		yield return new WaitForSeconds(turnDelay);
+		if (enemies.Count == 0) 
 		{
-			enemies.Add(script);
-		}
-		
-		public void GameOver()
-		{
-			levelText.text = "Your laptop died! You are stuck in the computer forever. GAME OVER.";
-			levelImage.SetActive(true);
-			enabled = false;
-		}
-		
-		IEnumerator MoveEnemies()
-		{
-			enemiesMoving = true;	
 			yield return new WaitForSeconds(turnDelay);
-			if (enemies.Count == 0) 
-			{
-				yield return new WaitForSeconds(turnDelay);
-			}
-			for (int i = 0; i < enemies.Count; i++)
-			{
-				if(enemies[i].health > 0) {
-					enemies[i].MoveEnemy();
-					yield return new WaitForSeconds(enemies[i].moveTime);
-				}
-			}
-			playersTurn = true;
-			enemiesMoving = false;
 		}
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			if(enemies[i].health > 0) {
+				enemies[i].MoveEnemy();
+				yield return new WaitForSeconds(enemies[i].moveTime);
+			}
+		}
+		playersTurn = true;
+		enemiesMoving = false;
+	}
 
 }
 
---
+++ TeamNameNotFoundException/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	public void PlayAgain() {
+		SceneManager.LoadScene ("CharacterSelection");
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+}
