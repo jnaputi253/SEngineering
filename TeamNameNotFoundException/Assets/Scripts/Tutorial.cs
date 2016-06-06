546f7122cf8c08ac890be5798f8b77e6a5a1bd05
ashleycorinne <amcbride@eagles.ewu.edu>
2016-06-05 17:55:51 -0700

    Tutorial scene added

---
+++ TeamNameNotFoundException/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour {
+
+    private int currentImage;
+    public Image[] images;
+	// Use this for initialization
+	void Start () {
+        currentImage = 0;
+        setImageFalse();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            setImageFalse();
+        }
+    }
+
+    private void setImageFalse()
+    {
+        if(currentImage == images.Length)
+        {
+           SceneManager.LoadScene("CharacterSelection");
+        }
+        foreach (Image im in images)
+        {
+            im.enabled = false;
+        }
+        if (currentImage < images.Length)
+        {
+            images[currentImage].enabled = true;
+            currentImage++;
+        }
+    }
+}
